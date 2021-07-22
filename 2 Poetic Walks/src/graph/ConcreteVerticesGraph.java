86f92c8e5a988b8283c778c5f9a91ccbbd7e1fd3
Junqi Xie <junqi_xie@outlook.com>
2021-07-22 22:45:52 +0800

    Implement and test `ConcreteVerticesGraph<String>`

--- 2 Poetic Walks/src/graph/ConcreteVerticesGraph.java
+++ 2 Poetic Walks/src/graph/ConcreteVerticesGraph.java
@@ -6,7 +6,10 @@ package graph;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
 
 /**
  * An implementation of Graph.
@@ -18,47 +21,127 @@ public class ConcreteVerticesGraph implements Graph<String> {
     private final List<Vertex> vertices = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    //   vertices represents the vertices in the graph, as well as the outgoing
+    //     edges from the vertices to target labels with weights
     // Representation invariant:
-    //   TODO
+    //   vertices don't contain vertices of the same label
     // Safety from rep exposure:
-    //   TODO
+    //   Vertex and Set<Vertex> are never returned, and are declared as private final
     
-    // TODO constructor
-    
-    // TODO checkRep
-    
+    /**
+     * Check the rep invariant.
+     */
+    private void checkRep() {
+        Set<String> labels = new HashSet<>();
+        for (Vertex vertex : vertices) {
+            String label = vertex.getLabel();
+            assert labels.contains(label) == false;
+            labels.add(label);
+        }
+    }
+
+    /**
+     * Find the vertex with label in vertices.
+     * 
+     * @param label label of the vertex
+     * @return the vertex with label
+     * @throws NoSuchElementException if not found
+     */
+    private Vertex findVertex(String label) throws NoSuchElementException {
+        for (Vertex vertex : vertices) {
+            if (label == vertex.getLabel()) {
+                return vertex;
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
     @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+        try {
+            findVertex(vertex);
+            return true;
+        }
+        catch (NoSuchElementException e) {
+            vertices.add(new Vertex(vertex));
+            checkRep();
+            return false;
+        }
     }
-    
+
     @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+        assert weight >= 0;
+        if (weight > 0) {
+            this.add(source);
+            this.add(target);
+        }
+
+        try {
+            int original = findVertex(source).set(target, weight);
+            checkRep();
+            return original;
+        }
+        catch (NoSuchElementException e) {
+            return 0;
+        }
     }
-    
+
     @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+        try {
+            vertices.remove(findVertex(vertex));
+            for (Vertex v : vertices) {
+                v.set(vertex, 0);
+            }
+            checkRep();
+            return true;
+        }
+        catch (NoSuchElementException e) {
+            return false;
+        }
     }
-    
+
     @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+        Set<String> result = new HashSet<>();
+        for (Vertex vertex : vertices) {
+            result.add(vertex.getLabel());
+            for (String target : vertex.getTargets().keySet()) {
+                result.add(target);
+            }
+        }
+        return result;
     }
-    
+
     @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+        Map<String, Integer> result = new HashMap<>();
+        for (Vertex vertex : vertices) {
+            Map<String, Integer> targets = vertex.getTargets();
+            if (targets.keySet().contains(target)) {
+                result.put(vertex.getLabel(), targets.get(target));
+            }
+        }
+        return result;
     }
-    
+
     @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+        return findVertex(source).getTargets();
     }
-    
-    // TODO toString()
+
+    /**
+     * @return a list of strings in the form "(source -> target, weight)",
+     *         representing the edges in the graph
+     */
+    @Override public String toString() {
+        String result = "";
+        for (Vertex vertex : vertices) {
+            result = result.concat(vertex.toString() + "\n");
+        }
+        return result.strip();
+    }
     
 }
 
 /**
- * TODO specification
- * Mutable.
+ * Vertex represents a vertex in a graph with a mutable map of outgoing edge
+ *   targets and weights.
  * This class is internal to the rep of ConcreteVerticesGraph.
  * 
  * <p>PS2 instructions: the specification and implementation of this class is
@@ -67,20 +150,98 @@ public class ConcreteVerticesGraph implements Graph<String> {
 class Vertex {
     
     // TODO fields
+    private final String label;
+    private final Map<String, Integer> targets = new HashMap<>();
     
     // Abstraction function:
-    //   TODO
+    //   represents a set of edges from label to targets with weights
     // Representation invariant:
-    //   TODO
+    //   weights in targets are positive integers
     // Safety from rep exposure:
-    //   TODO
+    //   String is immutable, and is declared as private final
+    //   Map<String, Integer> is copied when returned, and is declared as
+    //     private final
     
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    // TODO methods
-    
-    // TODO toString()
-    
+    /**
+     * Make a vertex with empty outgoing edge targets.
+     * 
+     * @param label label of the vertex
+     */
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    /**
+     * Check the rep invariant.
+     */
+    private void checkRep() {
+        for (int weight : targets.values()) {
+            assert weight > 0;
+        }
+    }
+
+    /**
+     * Get the label of the vertex.
+     * 
+     * @return a string of the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Get the outgoing edge targets and weights from the vertex.
+     * 
+     * @return a copy of the map containing outgoing edge targets and weights
+     */
+    public Map<String, Integer> getTargets() {
+        return new HashMap<>(targets);
+    }
+
+    /**
+     * Add, change, or remove a weighted directed edge from the vertex.
+     * If weight is nonzero, add an edge or update the weight of that edge.
+     * If weight is zero, remove the edge if it exists.
+     * 
+     * @param target label of the target vertex
+     * @param weight nonnegative weight of the edge
+     * @return the previous weight of the edge, or zero if there was no such
+     *         edge
+     */
+    public int set(String target, int weight) {
+        assert weight >= 0;
+
+        int original;
+        if (targets.containsKey(target)) {
+            original = targets.remove(target);
+        }
+        else {
+            original = 0;
+        }
+        if (weight > 0) {
+            targets.put(target, weight);
+        }
+
+        checkRep();
+        return original;
+    }
+
+    /**
+     * @return a list of strings in the form "(vertex -> target, weight)",
+     *         representing the edges outgoing from the vertex
+     */
+    @Override public String toString() {
+        String result = "";
+        Map<String, Integer> targets = this.getTargets();
+        for (String target : targets.keySet()) {
+            result = result.concat(
+                "(" + this.getLabel()
+                + " -> " + target
+                + ", " + targets.get(target)
+                + ")\n"
+            );
+        }
+        return result.strip();
+    }
+
 }
--- 2 Poetic Walks/test/graph/ConcreteVerticesGraphTest.java
+++ 2 Poetic Walks/test/graph/ConcreteVerticesGraphTest.java
@@ -5,6 +5,9 @@ package graph;
 
 import static org.junit.Assert.*;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.junit.Test;
 
 /**
@@ -29,17 +32,120 @@ public class ConcreteVerticesGraphTest extends GraphInstanceTest {
      */
     
     // Testing strategy for ConcreteVerticesGraph.toString()
-    //   TODO
+    //   edges in graph: 0, 1, >1
     
-    // TODO tests for ConcreteVerticesGraph.toString()
+    @Test
+    public void testVerticesGraphToStringEmpty() {
+        assertEquals("expected empty graph to string",
+                "", new ConcreteVerticesGraph().toString());
+    }
+
+    @Test
+    public void testVerticesGraphToStringSingleEdge() {
+        Graph<String> graph = new ConcreteVerticesGraph();
+        graph.set("source", "target", 1);
+        assertEquals("expected graph with a single edge to string",
+                "(source -> target, 1)", graph.toString());
+    }
+
+    @Test
+    public void testVerticesGraphToStringMultipleEdges() {
+        Graph<String> graph = new ConcreteVerticesGraph();
+        graph.set("source", "target", 1);
+        graph.set("target", "source", 2);
+        assertEquals("expected graph with multiple edges to string",
+                "(source -> target, 1)\n(target -> source, 2)", graph.toString());
+    }
     
     /*
      * Testing Vertex...
      */
     
     // Testing strategy for Vertex
-    //   TODO
+    //   set(target, weight)
+    //     target: exists, doesn't exist
+    //     source equals target or doesn't
+    //   toString()
+    //     targets number: 0, 1, >1
     
-    // TODO tests for operations of Vertex
-    
+    @Test
+    public void testVertexAddEdge() {
+        Vertex vertex = new Vertex("source");
+        int result = vertex.set("target", 1);
+        assertEquals("expected edge from source",
+                Map.of("target", 1), vertex.getTargets());
+        assertEquals("expected no such edge", 0, result);
+    }
+
+    @Test
+    public void testVertexAddLoop() {
+        Vertex vertex = new Vertex("init");
+        int result = vertex.set("init", 1);
+        assertEquals("expected edge from init",
+                Map.of("init", 1), vertex.getTargets());
+        assertEquals("expected no such edge", 0, result);
+    }
+
+    @Test
+    public void testVertexChange() {
+        Vertex vertex = new Vertex("source");
+        vertex.set("target", 1);
+        int result = vertex.set("target", 2);
+        assertEquals("expected edge from source",
+                Map.of("target", 2), vertex.getTargets());
+        assertEquals("expected previous weight of edge", 1, result);
+    }
+
+    @Test
+    public void testVertexRemoveEdge() {
+        Vertex vertex = new Vertex("source");
+        vertex.set("target", 1);
+        int result = vertex.set("target", 0);
+        assertEquals("expected no edges from source",
+                Collections.emptyMap(), vertex.getTargets());
+        assertEquals("expected previous weight of edge", 1, result);
+    }
+
+    @Test
+    public void testVertexRemoveEmpty() {
+        Vertex vertex = new Vertex("source");
+        int result = vertex.set("target", 0);
+        assertEquals("expected no edges from source",
+                Collections.emptyMap(), vertex.getTargets());
+        assertEquals("expected no such edge", 0, result);
+    }
+
+    @Test
+    public void testVertexRemoveLoop() {
+        Vertex vertex = new Vertex("init");
+        vertex.set("init", 1);
+        int result = vertex.set("init", 0);
+        assertEquals("expected no edges from init",
+                Collections.emptyMap(), vertex.getTargets());
+        assertEquals("expected previous weight of edge", 1, result);
+    }
+
+    @Test
+    public void testVertexToStringEmpty() {
+        assertEquals("expected empty graph to string",
+                "", new Vertex("init").toString());
+    }
+
+    @Test
+    public void testEdgesGraphToStringSingleEdge() {
+        Vertex vertex = new Vertex("source");
+        vertex.set("target", 1);
+        assertEquals("expected graph with a single edge to string",
+                "(source -> target, 1)", vertex.toString());
+    }
+
+    @Test
+    public void testEdgesGraphToStringMultipleEdges() {
+        Vertex vertex = new Vertex("source");
+        vertex.set("target", 1);
+        vertex.set("source", 2);
+        assertEquals("expected graph with multiple edges to string",
+                "(source -> source, 2)\n(source -> target, 1)", vertex.toString());
+    }
+
 }
