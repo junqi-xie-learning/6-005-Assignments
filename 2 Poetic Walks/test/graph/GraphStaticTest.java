95ef1179a50b56868e0b8fc64f38f3355c5eb71a
Junqi Xie <junqi_xie@outlook.com>
2021-07-23 22:22:24 +0800

    Test generic `Graph`

--- 2 Poetic Walks/test/graph/GraphStaticTest.java
+++ 2 Poetic Walks/test/graph/GraphStaticTest.java
@@ -6,6 +6,9 @@ package graph;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.math.BigInteger;
 
 import org.junit.Test;
 
@@ -21,6 +24,12 @@ public class GraphStaticTest {
     //   empty()
     //     no inputs, only output is empty graph
     //     observe with vertices()
+    //   Graph<String>
+    //     add(), set(), remove(), vertices(), sources(), targets(), toString()
+    //   Graph<Integer>
+    //     add(), set(), remove(), vertices(), sources(), targets(), toString()
+    //   Graph<BigInteger>
+    //     add(), set(), remove(), vertices(), sources(), targets(), toString()
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
@@ -32,7 +41,74 @@ public class GraphStaticTest {
         assertEquals("expected empty() graph to have no vertices",
                 Collections.emptySet(), Graph.empty().vertices());
     }
-    
-    // TODO test other vertex label types in Problem 3.2
-    
+
+    @Test
+    public void testGraphString() {
+        Graph<String> graph = Graph.empty();
+        graph.add("init");
+        graph.set("init", "init", 1);
+        graph.set("source", "target", 2);
+        graph.set("target", "source", 3);
+        graph.remove("target");
+        graph.set("init", "source", 4);
+        assertEquals("expected source vertex in the graph",
+                Set.of("init", "source"), graph.vertices());
+        assertEquals("expected edges from init",
+                Map.of("init", 1, "source", 4), graph.targets("init"));
+        assertEquals("expected edge to init",
+                Map.of("init", 1), graph.sources("init"));
+        assertEquals("expected no edges from source",
+                Collections.emptyMap(), graph.targets("source"));
+        assertEquals("expected edges to source",
+                Map.of("init", 4), graph.sources("source"));
+        assertEquals("expected graph with multiple edges to string",
+                "(init -> init, 1)\n(init -> source, 4)", graph.toString());
+    }
+
+    @Test
+    public void testGraphInteger() {
+        Graph<Integer> graph = Graph.empty();
+        graph.add(0);
+        graph.set(0, 0, 1);
+        graph.set(1, 2, 2);
+        graph.set(2, 1, 3);
+        graph.remove(2);
+        graph.set(0, 1, 4);
+        assertEquals("expected source vertex in the graph",
+                Set.of(0, 1), graph.vertices());
+        assertEquals("expected edges from init",
+                Map.of(0, 1, 1, 4), graph.targets(0));
+        assertEquals("expected edge to init",
+                Map.of(0, 1), graph.sources(0));
+        assertEquals("expected no edges from source",
+                Collections.emptyMap(), graph.targets(1));
+        assertEquals("expected edges to source",
+                Map.of(0, 4), graph.sources(1));
+        assertEquals("expected graph with multiple edges to string",
+                "(0 -> 0, 1)\n(0 -> 1, 4)", graph.toString());
+    }
+
+    @Test
+    public void testGraphBigInteger() {
+        Graph<BigInteger> graph = Graph.empty();
+        graph.add(BigInteger.ZERO);
+        graph.set(BigInteger.ZERO, BigInteger.ZERO, 1);
+        graph.set(BigInteger.ONE, BigInteger.TWO, 2);
+        graph.set(BigInteger.TWO, BigInteger.ONE, 3);
+        graph.remove(BigInteger.TWO);
+        graph.set(BigInteger.ZERO, BigInteger.ONE, 4);
+        assertEquals("expected source vertex in the graph",
+                Set.of(BigInteger.ZERO, BigInteger.ONE), graph.vertices());
+        assertEquals("expected edges from init",
+                Map.of(BigInteger.ZERO, 1, BigInteger.ONE, 4), graph.targets(BigInteger.ZERO));
+        assertEquals("expected edge to init",
+                Map.of(BigInteger.ZERO, 1), graph.sources(BigInteger.ZERO));
+        assertEquals("expected no edges from source",
+                Collections.emptyMap(), graph.targets(BigInteger.ONE));
+        assertEquals("expected edges to source",
+                Map.of(BigInteger.ZERO, 4), graph.sources(BigInteger.ONE));
+        assertEquals("expected graph with multiple edges to string",
+                "(0 -> 0, 1)\n(0 -> 1, 4)", graph.toString());
+    }
+
 }
